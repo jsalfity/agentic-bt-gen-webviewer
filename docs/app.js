e94f468c36f339fed4c06ecc405bbaa97992ac17
jsalfity <j.salfity@utexas.edu>
2026-07-10 14:42:01 -0700

    public website with info

--- docs/app.js
+++ docs/app.js
@@ -1,7 +1,7 @@
-const ANONYMOUS_MODE = true;
+const ANONYMOUS_MODE = false;
 
-const AUTHORS = 'Anonymous Authors';
-const AFFIL   = 'Anonymous Institution';
+const AUTHORS = 'Jonathan Salfity, Robert Blake Anderson, and Mitch Pryor';
+const AFFIL   = 'Nuclear and Applied Robotics Group · The University of Texas at Austin';
 
 const DATA_URL = 'data/catalog.json';
 
